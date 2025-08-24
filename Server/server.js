d1e63af6b4ea50c9ff5f3e0993fab70872c6c8c8
Ankitachaudhary-24 <ac0916218@gmail.com>
2025-08-24 22:00:48 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -28,7 +28,8 @@ app.use(
   })
 );
 
-
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/user',userRoutes);
