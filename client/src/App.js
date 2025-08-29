c1be3455850d8ed8f14489bf6d907329b896e91b
Ankitachaudhary-24 <ac0916218@gmail.com>
2025-08-29 13:01:19 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import MainLayout from "./pages/AnkitaPages/layouts/MainLayout";
 import Home from "./pages/AnkitaPages/Home";
 import UserLogin from "./pages/AnkitaPages/Login/UserLogin";
 import UserSignup from "./pages/AnkitaPages/Register/UserSignup";
+
 import CompanyLogin from "./pages/AnkitaPages/Login/CompanyLogin";
 import CompanySignup from "./pages/AnkitaPages/Register/CompanySignup";
 import CompanyDashboard from "./pages/AnkitaPages/COMPANY/CompanyDashboard";
@@ -16,35 +17,45 @@ import SecuritySettings from "./pages/AnkitaPages/COMPANY/Settings/SecuritySetti
 import SupportSettings from "./pages/AnkitaPages/COMPANY/Settings/SupportSettings.jsx";
 import TeamSettings from "./pages/AnkitaPages/COMPANY/Settings/TeamSettings.jsx";
 
-
-
-
-import JobPosting from"./pages/AnkitaPages/COMPANY/JobPosting";
-import ManageJobs from"./pages/AnkitaPages/COMPANY/ManageJobs";
-import EditJob from"./pages/AnkitaPages/COMPANY/EditJob.jsx";
-import JobDetails from"./pages/AnkitaPages/COMPANY/JobDetails";
-import ApplicantsHub from"./pages/AnkitaPages/COMPANY/ApplicantsHub.jsx";
+import JobPosting from "./pages/AnkitaPages/COMPANY/JobPosting";
+import ManageJobs from "./pages/AnkitaPages/COMPANY/ManageJobs";
+import EditJob from "./pages/AnkitaPages/COMPANY/EditJob.jsx";
+import JobDetails from "./pages/AnkitaPages/COMPANY/JobDetails";
+import ApplicantsHub from "./pages/AnkitaPages/COMPANY/ApplicantsHub.jsx";
 import EditCompanyProfile from "./pages/AnkitaPages/COMPANY/EditCompanyProfile.jsx";
-//import FreelancerProfile from"./pages/AnkitaPages/COMPANY/FreelancerProfile";
 import CompanyProfile from "./pages/AnkitaPages/COMPANY/CompanyProfile";
+
+// Jobseeker layout + pages
+
+import Dashboard from "./pages/AnkitaPages/JOBSEEKER/Dashboard";
+import JobsList from "./pages/AnkitaPages/JOBSEEKER/Jobs/JobsList";
+import JobDetail from "./pages/AnkitaPages/JOBSEEKER/Jobs/JobDetail";
+import SavedJobs from "./pages/AnkitaPages/JOBSEEKER/Jobs/SavedJobs";
+
+import Applications from "./pages/AnkitaPages/JOBSEEKER/Applications";
+import Messages from "./pages/AnkitaPages/JOBSEEKER/Messages";
+import UserProfile from "./pages/AnkitaPages/JOBSEEKER/UserProfile";
+import EditUserProfile from "./pages/AnkitaPages/JOBSEEKER/EditUserProfile";
+import UserSettings from "./pages/AnkitaPages/JOBSEEKER/UserSettings";
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public + Main layout */}
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          {/* future pages like /jobs etc go here */}
+          <Route index element={<Home />} />
         </Route>
-        
+
+        {/* Auth */}
         <Route path="/user/login" element={<UserLogin />} />
         <Route path="/user/signup" element={<UserSignup />} />
         <Route path="/company/login" element={<CompanyLogin />} />
         <Route path="/company/signup" element={<CompanySignup />} />
-        <Route path="/company-dashboard" element={<CompanyDashboard />} />
 
-        {/* SETTINGS: parent + nested children */}
+        {/* Company */}
+        <Route path="/company-dashboard" element={<CompanyDashboard />} />
         <Route path="/company/Settings" element={<SettingsLayout />}>
           <Route index element={<CompanyProfile />} />
           <Route path="profile" element={<CompanyProfile />} />
@@ -54,22 +65,32 @@ function App() {
           <Route path="billing" element={<BillingSettings />} />
           <Route path="compliance" element={<ComplianceSettings />} />
           <Route path="support" element={<SupportSettings />} />
-          
         </Route>
         <Route path="/company/Change-Password" element={<ChangePassword />} />
-
-       
         <Route path="/company/job-post" element={<JobPosting />} />
+        <Route path="/company/jobs" element={<ManageJobs />} />
+        <Route path="/company/job/edit/:jobId" element={<EditJob />} />
+        <Route path="/company/profile/edit" element={<EditCompanyProfile />} />
+        <Route path="/company/jobs/:id" element={<JobDetails />} />
+        <Route path="/company/jobs/:id/applicants" element={<ApplicantsHub />} />
+        <Route path="/company/profile" element={<CompanyProfile />} />
 
-         <Route path="/company/jobs" element={<ManageJobs />} />
-         <Route path="/company/job/edit/:jobId" element={<EditJob />} />
-         <Route path="/company/profile/edit" element={<EditCompanyProfile />} />
+       {/* ---------- JOBSEEKER (component-level layout like Company) ---------- */}
+         <Route path="/user/dashboard" element={<Dashboard />} />
+         <Route path="/user/jobs" element={<JobsList />} />
+         <Route path="/user/jobs/:id" element={<JobDetail />} />
+         <Route path="/user/saved" element={<SavedJobs />} />
 
-         <Route path="/company/jobs/:id" element={<JobDetails />} />
-         <Route path="/company/jobs/:id/applicants" element={<ApplicantsHub />} />
-         
-         <Route path="/company/profile" element={<CompanyProfile />} />
+         <Route path="/user/applications" element={<Applications />} />
+         <Route path="/user/messages" element={<Messages />} />
+         <Route path="/user/messages/:conversationId" element={<Messages />} />
+         <Route path="/user/profile" element={<UserProfile />} />
+         <Route path="/user/profile/edit" element={<EditUserProfile />} />
+         <Route path="/user/settings" element={<UserSettings />} />
 
+
+        {/* Optional: 404 */}
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
   );
